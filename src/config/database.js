06b148965f8a7dd945984ec5a025d844b5897be6
AnaPaulaBrencic <anapaulabrencic@hotmail.com>
2021-10-27 23:08:30 -0300

    database

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,4 @@
 require('dotenv/config')
-/**
-const dbConfig = {
-  dialect: process.env.DIALECT,
-  host: process.env.HOST,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  define: {
-    timestamps: true,
-    underscored: true
-  }
-}
-
-export default dbConfig
-*/
 
 const dbConfig = {
   dialect: 'mysql',
